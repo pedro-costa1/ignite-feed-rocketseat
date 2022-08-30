c6ad21d4f6da97eda7ad624caa68490a0b36fc7d
Pedro Costa <pedro_babysco@outlook.com>
2022-08-29 22:59:25 -0300

    css modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 
 export function App() {
   return(
     <div>
+      <Header />
+
       <Post 
         author="Pedro Costa"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet natus, adipisci exercitationem, unde facilis enim error temporibus repudiandae sunt laborum commodi! Atque voluptatem impedit possimus, voluptas et repellat minima libero."/>
