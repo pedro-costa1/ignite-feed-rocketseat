720c9afc4d948a5935fa4aca1a6446a8593f1055
Pedro Costa <pedro_babysco@outlook.com>
2022-09-01 00:57:32 -0300

    sidebar finalizada

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,26 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return(
     <div>
       <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          <Post 
+            author="Pedro Costa"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet natus, adipisci exercitationem, unde facilis enim error temporibus repudiandae sunt laborum commodi! Atque voluptatem impedit possimus, voluptas et repellat minima libero."/>
 
-      <Post 
-        author="Pedro Costa"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet natus, adipisci exercitationem, unde facilis enim error temporibus repudiandae sunt laborum commodi! Atque voluptatem impedit possimus, voluptas et repellat minima libero."/>
-    </div>
+        </main>
+      </div>
+      </div>
   )
 }
